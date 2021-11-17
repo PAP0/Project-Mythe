9e6d5f059326dca45bc0a01caa30156ca0ce64ea
PAP0 <54799202+PAP0@users.noreply.github.com>
2021-11-17 23:43:17 +0100

    Update MineStone.cs

--- 3PIGS/Assets/Scripts/Resources/MineStone.cs
+++ 3PIGS/Assets/Scripts/Resources/MineStone.cs
@@ -21,6 +21,34 @@ public class MineStone : MonoBehaviour
         {
             stone[2].SetActive(true);
         }
+        if (mining >= 12)
+        {
+            stone[3].SetActive(true);
+        }
+        if (mining >= 15)
+        {
+            stone[4].SetActive(true);
+        }
+        if (mining >= 18)
+        {
+            stone[5].SetActive(true);
+        }
+        if (mining >= 21)
+        {
+            stone[6].SetActive(true);
+        }
+        if (mining >= 24)
+        {
+            stone[7].SetActive(true);
+        }
+        if (mining >= 27)
+        {
+            stone[8].SetActive(true);
+        }
+        if (mining >= 30)
+        {
+            stone[9].SetActive(true);
+        }
     }
 
     void OnTriggerStay(Collider collision)
@@ -28,7 +56,7 @@ public class MineStone : MonoBehaviour
         if (collision.tag == "Pig")
         {
             Debug.Log("touching");
-            if (Input.GetButtonDown("P2Mine"))
+            if (Input.GetButtonDown("P2Mine") || Input.GetButtonDown("P3Mine") || Input.GetButtonDown("P4Mine"))
             {
                 mining = mining + 1;
             }
